12b4c3cb896f2bab99eb5165e6ead33ff8285563
Taiwooye <Oyedokuntaiwo96@gmail.com>
2023-11-23 15:25:19 +0100

    x

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -59,11 +59,11 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full ">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
+            I am a frontend web developer with a passion for creating
             interactive and responsive web applications. I have experience
             working with JavaScript, Reactjs, Nextjs, Tailwind, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am excited to work with others to create amazing websites.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
